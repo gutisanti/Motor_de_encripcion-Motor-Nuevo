435e4f884deb341d8148b5a1796a04230be78fd2
Santiago <santiago.guti17correa@gmail.com>
2024-03-21 07:31:15 -0500

    Final too

--- Motor_de_encripcion-Motor-Nuevo/jdjdj.py
+++
@@ -1,9 +0,0 @@
-from MTO import encrypt_message, decrypt_message, EncryptionError, DecryptionError
-try:
-    # Llamamos a la función encrypt_message con una clave de menos de 4 caracteres
-    encrypt_message("Hola Mundo", "abc")
-    print("No se lanzó ninguna excepción")
-except EncryptionError as e:
-    print("Se lanzó una EncryptionError:", e)
-except Exception as ex:
-    print("Se lanzó una excepción diferente:", ex)
